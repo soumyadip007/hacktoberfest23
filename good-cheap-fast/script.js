c8b33647bb1f9b2b7ea5a7991314967219012ce3
Rigin Oommen <riginoommen@gmail.com>
2022-10-14 18:41:21 +0530

    chore - updated the lint issues

--- good-cheap-fast/script.js
+++ good-cheap-fast/script.js
@@ -1,22 +1,22 @@
-const toggles = document.querySelectorAll('.toggle')
-const good = document.querySelector('#good')
-const cheap = document.querySelector('#cheap')
-const fast = document.querySelector('#fast')
+const toggles = document.querySelectorAll('.toggle');
+const good = document.querySelector('#good');
+const cheap = document.querySelector('#cheap');
+const fast = document.querySelector('#fast');
 
-toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)));
 
 function doTheTrick(theClickedOne) {
     if(good.checked && cheap.checked && fast.checked) {
         if(good === theClickedOne) {
-            fast.checked = false
+            fast.checked = false;
         }
 
         if(cheap === theClickedOne) {
-            good.checked = false
+            good.checked = false;
         }
 
         if(fast === theClickedOne) {
-            cheap.checked = false
+            cheap.checked = false;
         }
     }
 }
